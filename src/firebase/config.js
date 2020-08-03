1d25bb0022358723348f664695c9933ff39a1329
sultanbayev <sultanbayev@gmail.com>
2020-08-03 22:50:09 +0600

    "checked new config"

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,3 @@
 export const config = {
-  apiKey: "AIzaSyA1gkIA3GUHnxMkFrAy2itcmoFTlWV7YM8",
-  authDomain: "postgram-38cad.firebaseapp.com",
-  databaseURL: "https://postgram-38cad.firebaseio.com",
-  projectId: "postgram-38cad",
-  storageBucket: "postgram-38cad.appspot.com",
-  messagingSenderId: "1029913541884",
-  appId: "1:1029913541884:web:d4fa3a2bda05313e43eb79",
+  //see notion (for security reasons)
 };
